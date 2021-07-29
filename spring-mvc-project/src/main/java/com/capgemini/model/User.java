e7b8598b94b2412a020e993adcc48ff98696cc3d
andrefer <andrefer@les007596.corp.capgemini.com>
2021-07-29 13:17:52 +0200

    Ejemplo de clase

---
+++ spring-mvc-project/src/main/java/com/capgemini/model/User.java
@@ -0,0 +1,24 @@
+package com.capgemini.model;
+
+public class User {
+	
+	private String userName;
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public User(String userName) {
+		super();
+		this.userName = userName;
+	}
+
+	public User() {
+		
+	}
+
+}
