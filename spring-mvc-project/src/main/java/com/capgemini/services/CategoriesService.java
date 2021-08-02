fa900e1ff8b75c72a8b4de43431629b24edf4cac
ivamarqu <kibi31?D>
2021-08-02 08:21:23 +0200

    "fix: categoriesChange"

--- spring-mvc-project/src/main/java/com/capgemini/services/CategoriesService.java
+++ spring-mvc-project/src/main/java/com/capgemini/services/CategoriesService.java
@@ -1,7 +1,10 @@
 package com.capgemini.services;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
+import com.capgemini.model.Category;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -16,8 +19,22 @@ public class CategoriesService {
 	
 
 	public void deleteCategory(int id) throws SQLException {
-		
 		categoriesRepository.delete(id);
+	}
+
+	public List<Category> getCategories() throws SQLException {
+
+		List<Object> categories = categoriesRepository.findAll();
+		List<Category> categories1 = new ArrayList<>();
+
+		for(Object u : categories) {
+			Category category = (Category) u;
+
+			Category category2 = new Category(category.id, category.name, category.user_id);
+
+			categories1.add(category2);
+		}
+		return categories1;
 	}
 
 }
